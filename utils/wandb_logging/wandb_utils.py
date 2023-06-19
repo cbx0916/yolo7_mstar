ea0cb8aee36947608813bf5edf0009aeadc0f42b
J.S.Aoxiang <aoxiang0916@126.com>
2023-06-19 17:23:41 +0800

    Update wandb_utils.py

--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -11,12 +11,12 @@ from utils.datasets import LoadImagesAndLabels
 from utils.datasets import img2label_paths
 from utils.general import colorstr, xywh2xyxy, check_dataset
 
-try:
-    import wandb
-    from wandb import init, finish
-except ImportError:
-    wandb = None
-
+# try:
+#     import wandb
+#     from wandb import init, finish
+# except ImportError:
+#     wandb = None
+wandb = None
 WANDB_ARTIFACT_PREFIX = 'wandb-artifact://'
 
 
